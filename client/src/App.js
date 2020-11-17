6aebe08f832dd46ae2bb99ae6cbc04345e8b6bd1
Anna <anna_agabekyan_anna@mail.ru>
2020-11-17 11:48:15 +0400

    feat: Setting up page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import 'materialize-css'
+import { userRoutes } from './routes'
 
 function App() {
+  const routes = userRoutes(true)
   return (
-      <div>
-         <h1>Hello</h1>
+    <Router>
+      <div className="container">
+         {routes}
       </div>
+    </Router>
   )
 }
 
